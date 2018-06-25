dbd7045b9f307568af3757933077fe0154e0834a
0x008800 <0x008800@gmail.com>
2018-06-25 19:51:41 +0300

    adding viewing of list by get request

--- src/main/frontend/task-manager/src/app/task.service.ts
+++ src/main/frontend/task-manager/src/app/task.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+import { Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
+
+
+import { Task } from './task';
+// import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
 
-  constructor() { }
+export class TaskService {
+  private taskUrl = 'http://localhost:8080/getall';
+
+
+  constructor(
+    private http: HttpClient,
+    // private  messageService: MessageService
+  ){  }
+
+    getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.taskUrl).pipe(
+      tap(tasks => this.log('fetched tasks')),
+      catchError(this.handleError('getTasks', []))
+      );
+    }
+
+
+    private log(message: string){
+      // this.messageService.add('TaskService: ' + message);
+    }
+
+    private handleError<T>(operation = 'operation', result?: T){
+
+    return (error: any): Observable<T> => {
+        console.error(error);
+
+        this.log('{$operation} failed: ${error.message}');
+
+        return of(result as T);
+      };
+    }
 }
--- src/main/frontend/task-manager/src/app/tasklist/tasklist.component.ts
+++ src/main/frontend/task-manager/src/app/tasklist/tasklist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
+import {TaskService} from "../task.service";
+import {Observable} from "rxjs/internal/Observable";
+import {T} from "@angular/core/src/render3";
 @Component({
   selector: 'app-tasklist',
   templateUrl: './tasklist.component.html',
@@ -7,13 +10,23 @@ import { Task } from '../task';
 })
 export class TasklistComponent implements OnInit {
 
-  tasks: Task[] = [{id: 1, startOfTask: '3-3-323', finishOfTask: '44-34-23', nameOfTask: 'nameee', contributors: 'alex'},
-                    {id: 2, startOfTask: '3-3-323', finishOfTask: '44-34-23', nameOfTask: 'nameee', contributors: 'alex'}
-                  ]
+  selectedTask: Task;
+  tasks: Task[];
+    // = [{id: 1, startOfTask: '3-3-323', finishOfTask: '44-34-23', nameOfTask: 'nameee', contributors: 'alex'},
+    //                 {id: 2, startOfTask: '3-3-323', finishOfTask: '44-34-23', nameOfTask: 'nameee', contributors: 'alex'}
+    //               ]
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.getTasks();
   }
 
+  getTasks(): void{
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
+
+  onSelect(task: Task): void{
+    this.selectedTask = task;
+  }
 }
